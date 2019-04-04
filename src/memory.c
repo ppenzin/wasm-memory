0cb4205dda8228ced9240ae2efc0db2f4d7c23d8
Petr Penzin <penzin.dev@gmail.com>
2019-04-03 18:26:54 -0700

    Better offsets
    Enforce minimal size rule to accomodate free list pointers and size value at
    the end of free chunk. Add a bit more descriptive comment to the source and
    expand the test.

--- src/memory.c
+++ src/memory.c
@@ -13,9 +13,44 @@ void * memset(void * ptr, int value, size_t num) {
   return ptr;
 }
 
+/* Simple memory allocator inspired by dlmalloc
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *
+ * Finds an unallocated memory chunk that fits requested size. Maintains a free
+ * list (deallocated chunks), starting pointer for which is placed right at the
+ * beginning of the heap.
+ *
+ * Allocated chunk looks like this:
+ * -----------------------------
+ * (size_t) chunk_size
+ * -----------------------------
+ * (void) data <-- return ptr
+ * -----------------------------
+ *
+ * Free chunk:
+ * -----------------------------
+ * (size_t) chunk_size
+ * -----------------------------
+ * (void *) previous
+ * -----------------------------
+ * (void *) next
+ * -----------------------------
+ * (size_t) chunk_size
+ * -----------------------------
+ *
+ * Free chunks storing size at the beginning and end is done for ease of
+ * merging two adjacent free chunks. After chunks is freed its size does not
+ * change, which imoses a minimal offset between starting pointers of two
+ * adjacent chunks. Free chunks are organized into a double linked list for
+ * ease of lookups.
+ */
+
 extern unsigned char __heap_base;
 
-/**** Simple memory allocator inspired by dlmalloc ****/
+/// Minimal offset between size of the current chunk and begining of the next
+/// one; accomodates two pointers needed for doubly-linked list of free chunks
+/// and extra size value placed at end of every free chunk
+const size_t min_chunk_offset = 2 * sizeof(void *) + sizeof(size_t);
 
 export
 void * malloc(size_t size) {
@@ -34,9 +69,11 @@ void * malloc(size_t size) {
 
     // Every chunk starts with its size (in bytes)
     size_t chunk_size = *(size_t*)current_chunk;
+    size_t offset = (chunk_size < min_chunk_offset) ? min_chunk_offset : chunk_size;
     while (chunk_size != 0) { // Loop through all used chunks
-      current_chunk = (unsigned char*)current_chunk + chunk_size;
+      current_chunk = (unsigned char*)current_chunk + offset;
       chunk_size = *(size_t*)current_chunk;
+      offset = (chunk_size < min_chunk_offset) ? min_chunk_offset : chunk_size;
     }
 
     // Set the first free chunk up to be used
--- test/malloc.js
+++ test/malloc.js
@@ -5,24 +5,50 @@ const module = new WebAssembly.Module(read('mem.wasm', 'binary'));
 const instance = new WebAssembly.Instance(module, { "env" : imports }).exports;
 var u8_data = new Uint8Array(imports["memory"]["buffer"]);
 
+// Convert 4 uint8 values to a single int32 value
 function uchar2int32(arr, off) {
-  // Convert 4 uint8 values to a single int32 value
   var u8arr = new Uint8Array([arr[off], arr[off+1], arr[off+2], arr[off+3]]);
   return new Int32Array(u8arr.buffer)[0];
 }
 
-function check(arr, start, count, value) {
-  for (let i = start; i < start + count; ++i) {
-    if (u8_data[i] != value)
-      return false;
-  }
-  return true;
-}
+var ptr = instance.malloc(6);
 
-var ptr = instance.malloc(256);
+/* Expected heap (starts at __heap_base):
+ * -----------------------
+ * (void *) free list == 0
+ * -----------------------
+ * (size_t) chunk size = 6
+ * -----------------------
+ * (void *) alloc == ptr
+ * -----------------------
+ */
+var passed = (uchar2int32(u8_data, instance.__heap_base) == 0)
+          && (uchar2int32(u8_data, instance.__heap_base + 4) == 6)
+          && (ptr == instance.__heap_base + 8);
 
-var passed = (ptr == instance.__heap_base + 8);
-passed = passed && (uchar2int32(u8_data, instance.__heap_base + 4) == 256);
+ptr = instance.malloc(4);
+
+/* Expected heap (starts at __heap_base):
+ * ------------------------
+ * (void *) free list == 0
+ * ------------------------
+ * (size_t) chunk size = 6
+ * ------------------------
+ * 6 bytes (previous alloc)
+ * + 6 bytes to get to
+ * 12 bytes required
+ * between chunks
+ * ------------------------
+ * (size_t) chunk size = 4
+ * ------------------------
+ * (void *) alloc == ptr
+ * ------------------------
+ */
+
+passed = passed && (uchar2int32(u8_data, instance.__heap_base) == 0)
+                && (uchar2int32(u8_data, instance.__heap_base + 4) == 6)
+                && (uchar2int32(u8_data, instance.__heap_base + 16) == 4)
+                && (ptr == instance.__heap_base + 20);
 
 if (passed)
   print("PASS");
