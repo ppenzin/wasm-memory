dfdf604baed24c62f8c5a9896d5ff763f7f5882a
Petr Penzin <penzin.dev@gmail.com>
2019-08-02 20:02:11 -0700

    Append to free list

--- src/memory.c
+++ src/memory.c
@@ -208,22 +208,30 @@ void free(void * ptr) {
   // FIXME Merge with adjacent chunk if that is free
 
   // Find first free chunk of the same or greater size
-  free_chunk_header * next = find_free_chunk(*list, effective_size);
+  free_chunk_header * chunk = *list;
+  free_chunk_header * last = 0;
 
-  // FIXME tail chunk (currently next == NULL && *list != NULL)
+  while (chunk && chunk->size < effective_size) {
+    if (chunk->next == 0)
+      last = chunk;
+    chunk = chunk->next;
+  }
 
-  if (next == *list) { // First or smallest chunk
-    hdr->next = *list;
+  if (chunk == *list) { // First or smallest chunk
+    hdr->next= *list;
     *list = hdr;
     if (hdr->next)
       ((free_chunk_header *)hdr->next)->previous = hdr;
+  } else if (!chunk) { // Append at the end
+    hdr->previous = last;
+    last->next = hdr;
   } else {
-    hdr->next = next;
-    hdr->previous = next->previous;
+    hdr->next = chunk;
+    hdr->previous = chunk->previous;
     if (hdr->previous)
       ((free_chunk_header *)hdr->previous)->next = hdr;
-    next->previous = hdr;
-    if (next == *list)
+    chunk->previous = hdr;
+    if (chunk == *list)
       *list = hdr;
   }
 
